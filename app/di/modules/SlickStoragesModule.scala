a04e8b6ca2e047e7638b0c6d648483307529d6cc
HeavyPunk <kirillkuchma8@gmail.com>
2023-11-22 22:17:05 +0500

    Init database when loading di module

--- app/di/modules/SlickStoragesModule.scala
+++ app/di/modules/SlickStoragesModule.scala
@@ -11,6 +11,7 @@ import business.services.slickStorages.tariff.TariffStorage
 import business.services.slickStorages.user.SlickUserStorage
 import business.services.slickStorages.user.UserStorage
 import com.google.inject.AbstractModule
+import components.services.database.SlickDatabaseInitializer
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.duration.Duration
@@ -19,6 +20,9 @@ class SlickStoragesModule extends AbstractModule {
     override def configure() = {
         val db = Database.forConfig("database")
         val operationTimeout = Duration.create(10, "sec")
+        val databaseInitializer = SlickDatabaseInitializer()
+        databaseInitializer.initDatabase(db, operationTimeout)
+
         val userStorage = SlickUserStorage(db, operationTimeout)
         val gamesStorage = SlickGameStorage(db, operationTimeout)
         val locationsStorage = SlickLocationsStorage(db, operationTimeout)
