3fde4ff82dd1f195a81a415800099ca352d718ee
HeavyPunk <kirillkuchma8@gmail.com>
2023-11-24 02:17:53 +0500

    Update prod-settings

--- .ci/docker-prod.sh
+++ .ci/docker-prod.sh
@@ -3,6 +3,6 @@
 bash publish-prod.sh
 
 DOCKER_IMAGE=kirieshki/simple-hosting.web2
-DOCKER_TAG="2023.11.06.1"
+DOCKER_TAG="2023.11.24.1"
 docker build -t $DOCKER_IMAGE:$DOCKER_TAG .
 docker push $DOCKER_IMAGE:$DOCKER_TAG
