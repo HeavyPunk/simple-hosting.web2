644305723eaa1f57007ce5b8c05fe9327bf0dad7
HeavyPunk <kirillkuchma8@gmail.com>
2023-12-03 00:29:46 +0500

    Docker buildx using

--- .ci/docker-prod.sh
+++ .ci/docker-prod.sh
@@ -5,6 +5,5 @@ set -eu
 bash publish-prod.sh
 
 DOCKER_IMAGE=kirieshki/simple-hosting.web2
-DOCKER_TAG="2023.11.27.2"
-docker build -t $DOCKER_IMAGE:$DOCKER_TAG .
-docker push $DOCKER_IMAGE:$DOCKER_TAG
+DOCKER_TAG="2023.12.02.1"
+docker buildx build -t $DOCKER_IMAGE:$DOCKER_TAG --progress plain --platform linux/amd64,linux/arm64,linux/arm64/v8 --push .
